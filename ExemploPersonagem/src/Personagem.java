f8e36e3a443cb1905ec2ee4620baf658db267ac4
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-06-12 09:00:08 -0300

    Exemplo finalizado Personagem

--- ExemploPersonagem/src/Personagem.java
+++ ExemploPersonagem/src/Personagem.java
@@ -1,13 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Alunos
+ * @author Logan Michel
  */
 public class Personagem {
     
+     private String nome, cla, aldeia;
+    private long nivelChakra;
+    private char sexo;
+    private byte idade, idadeMental;
+    
+    //permitir guardar informação
+    public void setNome(String x1){
+        if(x1.trim().equals("")){
+            throw new
+            IllegalArgumentException("Nome não pode ser vazio!");
+        }
+        if(x1.trim().length() < 3){
+            throw new
+            IllegalArgumentException("Nome tem que contem no minimo 3 caracteres");
+            
+        }
+        this.nome = x1;
+    }
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setCla (String cla){
+        this.cla = cla;
+        
+    }
+    
+    public String getCla(){
+        return cla;
+    }
+    
+    public void setAldeia (String nomeAldeia){
+        this.aldeia = nomeAldeia;
+    }
+    
+    public String getAldeia(){
+        return aldeia;
+        
+    }
+    
+    public void setNivelChakra(long nivelChakra){
+        this.nivelChakra = nivelChakra;
+    }
+    
+    public long getNivelChakra(){
+        return nivelChakra;
+    }
+    
+    public void setSexo(char sexo){
+        this.sexo = sexo;
+    }
+    
+    public char getSexo(){
+        return sexo;
+    }
+    
+    public void setIdade(byte idade){
+        this.idade = idade;
+    }
+    
+    public byte getIdade(){
+        return idade;
+    }
+    
+    public void setIdadeMental(byte idadeMental){
+        this.idadeMental = idadeMental;
+    }
+    
+    public byte getIdadeMental(){
+        return idadeMental;
+    }
 }
--- ExemploPersonagem/test/PersonagemJUnitTest.java
+++ ExemploPersonagem/test/PersonagemJUnitTest.java
@@ -1,19 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
  * @author Alunos
  */
 public class PersonagemJUnitTest {
     
-    public PersonagemJUnitTest() {
+    @Test
+    public void testeDefinirEObterNome() {
+        Personagem sakura = new Personagem();
+        sakura.setNome("Sakura");
+        assertEquals("Sakura", sakura.getNome());
+        sakura.setNome("Sakura Haruno");
+        assertEquals("Sakura Haruno", sakura.getNome());
     }
     
+    @Test
+    public void testeDefinirEObter() {
+        Personagem choji = new Personagem();
+        choji.setNome("Choji");
+        choji.setIdade((byte) 17);
+        assertEquals(17, choji.getIdade());
+    }
+    
+    @Test
+    public void testeDefinirEObterNivelSexo() {
+        Personagem zabuza = new Personagem();
+        zabuza.setSexo('m');
+        assertEquals('m', zabuza.getSexo());
+    }
+    
+    @Test
+    public void testeDefinirEObterNivelChakra() {
+        Personagem deidara = new Personagem();
+        deidara.setNivelChakra(500);
+        assertEquals(500, deidara.getNivelChakra());
+        
+    }
+    
+    @Test
+    public void testeDefinirEObterAldeia() {
+        Personagem rookLee = new Personagem();
+        rookLee.setAldeia("Folha");
+        assertEquals("Folha", rookLee.getAldeia());
+        
+    }
+    
+    @Test
+    public void testeDefinirEObterCla() {
+        Personagem hinata = new Personagem();
+        hinata.setCla("Hyuuga");
+        assertEquals("Hyuuga", hinata.getCla());
+        
+    }
+    
+    @Test
+    public void testeDefinirEObterIdadeMental(){
+        Personagem jiraya = new Personagem();
+        jiraya.setIdadeMental((byte) -10);
+        assertEquals(-10, jiraya.getIdadeMental());
+    }
 }
